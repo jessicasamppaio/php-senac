23345223ae87e302bbdab8550dfdb634e381a050
Jéssica Sampaio <jessicaspf@gmail.com>
2024-03-21 20:31:06 -0300

    Atualiazando com aula 02

--- 02/index.php
+++ 02/index.php
@@ -10,8 +10,12 @@ echo $nome." ".$sobrenome; // Saída: Jéssica Sampaio
 echo "<br>";
 
 # Para obter o comprimento de uma string em PHP, usamos a função strlen()
-$texto = "Olá, mundo";
-echo strlen($texto);
+$texto = "Olá, mundo!";
+echo strlen($texto); // Saída: 12
+echo "<br>";
+
+# Para substituir partes de uma string, usamos a função str_replace()
+$texto = "O rato roeu a roupa do rei";
 
 
 ?>
