af91f4b2eabdfa143ccaf5b5b060f89cfa40f41f
Jéssica Sampaio <jessicaspf@gmail.com>
2024-03-21 20:14:01 -0300

    Atuaizando com aula 01

--- 01/teste.php
+++ 01/teste.php
@@ -27,7 +27,7 @@
     $ano = 2024;
 
     echo $idade . "<br>";
-    echo $ano - $idade; // operações matematicas:
+    echo $ano - $idade; // operações matematicas
 
     echo 'Esse é um texto em aspas simples <br>';
     echo "Esse é um texto em aspas duplas <br>";
@@ -35,5 +35,15 @@
     // Trabalhando com aspas simples e duplas:
     echo 'Dom Pedro isse: "Inependencia ou Morte!" <br>';
     echo "$nome tem $idade anos de idade <br>";
-    echo '$nome tem $idade anos de idade';
+    echo '$nome tem $idade anos de idade <br>';
+
+    // Testando o case sensitive:
+    echo "Texto 1 <br>";
+    ECHO "Texto 2 <br>";
+
+    // Testando o case sensitive - variáveis:
+        $primeiroNome = "Carlos";
+
+        echo $primeiroNome;
+        ECHO $PRIMEIRONOME;
 ?>
