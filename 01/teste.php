7a75251c2f52bbc06552d8edf8ccad29f51abffe
Jéssica Sampaio <jessicaspf@gmail.com>
2024-03-20 22:03:12 -0300

    Testanto

--- 01/teste.php
+++ 01/teste.php
@@ -1,5 +1,25 @@
 <?php
 
-    echo "Olá Mundo!";
+    echo "Olá Mundo! <BR>";
+
+    // Isso é um comentário
+
+    # Isso também é um comentário
+
+    /*
+        Isso é um comentário em multiplas linhas
+        Eu disse, multiplas linhas
+    */
+
+    // Declaração de variáveis
+    $nome = "João";
+    $sobrenome = "Silva";
+    $nome_completo = $nome ." ". $sobrenome;
+
+
+    //echo $nome;
+    //echo $sobrenome;
+    echo $nome ." ". $sobrenome;
+    echo $nome_completo;
 
 ?>
