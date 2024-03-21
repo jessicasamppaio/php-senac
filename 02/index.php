b46826e70d4fa8f37b42ed2c2c914694435eb49f
Jéssica Sampaio <jessicaspf@gmail.com>
2024-03-21 20:25:49 -0300

    Atualiazação da aula 02

--- 02/index.php
+++ 02/index.php
@@ -7,6 +7,11 @@ $nome = "Jéssica";
 $sobrenome = "Sampaio";
 
 echo $nome." ".$sobrenome; // Saída: Jéssica Sampaio
+echo "<br>";
+
+# Para obter o comprimento de uma string em PHP, usamos a função strlen()
+$texto = "Olá, mundo";
+echo strlen($texto);
 
 
 ?>
