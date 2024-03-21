743a7bf65367613b2112e5e489e64c9a422e92df
Jéssica Sampaio <jessicaspf@gmail.com>
2024-03-21 20:18:21 -0300

    Atualiando com aula 02

--- 02/index.php
+++ 02/index.php
@@ -1,4 +1,12 @@
 <?php
-// Strings
+# Strings
+
+
+# Para unir duas ou mais strings em PHP, usamos o operador de concatenação.
+$nome = "Jéssica";
+$sobrenome = "Sampaio";
+
+echo $nome." ".$sobrenome; // Saída: Jéssica Sampaio
+
 
 ?>
