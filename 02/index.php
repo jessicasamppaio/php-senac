e93600f0c5839a229a301ccb4c47580edafce178
Jéssica Sampaio <jessicaspf@gmail.com>
2024-03-21 20:48:29 -0300

    Atauliazando com str_replace

--- 02/index.php
+++ 02/index.php
@@ -18,6 +18,22 @@ echo "<br>";
 $texto = "O rato roeu a roupa do rei";
 $novo_texto = str_replace("rato", "gato", $texto);
 echo $novo_texto; // Saída: O gato roeu a roupa do rei.
+echo "<br>";
+
+# strtolower(): Converte uma string para letras minúsculas.
+# strtoupper(): Converte uma string para letras maiúscula.
+$texto = "Olá, mundo!";
+$texto_minusculo = strtolower($texto);
+$texto_maiusculo = strtoupper($texto);
+
+echo $texto_minusculo;
+echo "<br>";
+
+echo $texto_maiusculo;
+echo "<br>";
+
+echo $texto;
+
 
 
 ?>
